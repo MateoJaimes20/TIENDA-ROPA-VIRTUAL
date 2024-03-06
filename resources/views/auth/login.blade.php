3a345adc4f6c049ee4a6ee32771898a5fda62428
Alcinaviernes <dfalcinaq@ufpso.edu.co>
2024-03-06 15:29:47 -0500

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.applogin')
+
+@section('tittle','Login')
+
+@section('content')
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="../../index2.html" class="h1"><b>Grove</b>Street</a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Iniciar Sesion</p>
+
+      <form method="POST" action="{{ route('login') }}">
+      @csrf
+        <div class="input-group mb-3">
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                    </div>
+                </div>
+        </div>
+        <div class="input-group mb-3">
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                    </div>
+                </div>
+        </div>
+        <div class="row">
+          
+          <!-- /.col -->
+          <div class="col-6">
+            <button type="submit" class="btn btn-primary btn-block">ingresar</button>
+          </div>
+          
+          <div class="col-6">
+            <a href="{{route('register')}}" class="btn btn-primary btn-block">Registrar</a>
+          </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-12">
+                <p class="mb-1">
+                @if (Route::has('password.request'))
+                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                        {{ __('Olvide mi contraseña') }}
+                    </a>
+                @endif
+                </p>
+            </div>
+        </div>
+     
+
+
+      </form>
+
+      
+      
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+@endsection
